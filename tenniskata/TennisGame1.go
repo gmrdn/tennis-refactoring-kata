4eb6d670fa1b01154440a5012f0e626849be7db1
Guillaume R <guillaume.rondon@gmail.com>
2020-06-06 20:50:11 +0200

    Started refactoring first kata

--- tenniskata/TennisGame1.go
+++ tenniskata/TennisGame1.go
@@ -1,12 +1,13 @@
 package tenniskata
 
 type tennisGame1 struct {
-	m_score1    int
-	m_score2    int
+	mScore1    int
+	mScore2    int
 	player1Name string
 	player2Name string
 }
 
+// TennisGame1 Starts a tennis game with two players names
 func TennisGame1(player1Name string, player2Name string) TennisGame {
 	game := &tennisGame1{
 		player1Name: player1Name,
@@ -16,29 +17,40 @@ func TennisGame1(player1Name string, player2Name string) TennisGame {
 }
 
 func (game *tennisGame1) WonPoint(playerName string) {
-	if playerName == "player1" {
-		game.m_score1 += 1
+	if playerName == game.player1Name {
+		game.mScore1++
 	} else {
-		game.m_score2 += 1
+		game.mScore2++
 	}
 }
+
+func (game *tennisGame1) isEqualScore() bool {
+	return game.mScore1 == game.mScore2
+}
+
+func (game *tennisGame1) getEqualScore(score int) string {
+	textualScore := ""
+	switch score {
+	case 0:
+		textualScore = "Love-All"
+	case 1:
+		textualScore = "Fifteen-All"
+	case 2:
+		textualScore = "Thirty-All"
+	default:
+		textualScore = "Deuce"
+	}
+	return textualScore
+}
+
 
 func (game *tennisGame1) GetScore() string {
 	score := ""
 	tempScore := 0
-	if game.m_score1 == game.m_score2 {
-		switch game.m_score1 {
-		case 0:
-			score = "Love-All"
-		case 1:
-			score = "Fifteen-All"
-		case 2:
-			score = "Thirty-All"
-		default:
-			score = "Deuce"
-		}
-	} else if game.m_score1 >= 4 || game.m_score2 >= 4 {
-		minusResult := game.m_score1 - game.m_score2
+	if game.isEqualScore() {
+		score = game.getEqualScore(game.mScore1)
+	} else if game.mScore1 >= 4 || game.mScore2 >= 4 {
+		minusResult := game.mScore1 - game.mScore2
 		if minusResult == 1 {
 			score = "Advantage player1"
 		} else if minusResult == -1 {
@@ -51,10 +63,10 @@ func (game *tennisGame1) GetScore() string {
 	} else {
 		for i := 1; i < 3; i++ {
 			if i == 1 {
-				tempScore = game.m_score1
+				tempScore = game.mScore1
 			} else {
 				score += "-"
-				tempScore = game.m_score2
+				tempScore = game.mScore2
 			}
 			switch tempScore {
 			case 0:
